2485c7dca75256a0391c39c9202d8fdb03a4cc0f
mekk1t <kit.kalimov@gmail.com>
2021-04-28 20:12:57 +0300

    Расширение для конвертации Bitmap в BitmapImage

---
+++ ImageClusters/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace ImageClusters
+{
+    public static class Extensions
+    {
+        public static BitmapImage ToBitmapImage(this Bitmap bitmap)
+        {
+            using var ms = new MemoryStream();
+            bitmap.Save(ms, ImageFormat.Bmp);
+            ms.Position = 0;
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = ms;
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.EndInit();
+
+            return bitmapImage;
+        }
+    }
+}
