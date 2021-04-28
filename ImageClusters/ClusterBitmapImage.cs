da58dd358124c1656e474f329056054810fa926f
mekk1t <kit.kalimov@gmail.com>
2021-04-28 22:44:34 +0300

    Кластер как бинарное изображение + маппинг результатов работы нейронной сети

---
+++ ImageClusters/ClusterBitmapImage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace ImageClusters
+{
+    public class ClusterBitmapImage : List<BitmapImage>, IGrouping<BitmapImage, BitmapImage>
+    {
+        public ClusterBitmapImage(BitmapImage key) : base() => Key = key;
+
+        public ClusterBitmapImage(BitmapImage key, IEnumerable<BitmapImage> collection) : base(collection) => Key = key;
+
+        public BitmapImage Key { get; }
+    }
+}
--- ImageClusters/ViewModels/MainWindowViewModel.cs
+++ ImageClusters/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Windows.Media.Imaging;
+using Tools;
 
 namespace ImageClusters.ViewModels
 {
@@ -12,10 +13,27 @@ namespace ImageClusters.ViewModels
         public List<BitmapImage> ImagesStart { get; set; }
         public List<BitmapImage> ImagesBinarized { get; set; }
 
+        public List<ClusterBitmapImage> Clusters { get; set; }
+
         public MainWindowViewModel()
         {
             ImagesStart = ReadIcons("Images");
-            ImagesBinarized = ReadIconsAsBitmap("Images").Select(icon => new ImageBinarizer(icon).ImageBytes.CreateBitmap().ToBitmapImage()).ToList();
+            var imagesBinarized = ReadIconsAsBitmap("Images").Select(icon => new ImageBinarizer(icon)).ToList();
+            ImagesBinarized = imagesBinarized.Select(icon => icon.ImageBytes.CreateBitmap().ToBitmapImage()).ToList();
+            var imagesBinarizedBytes = imagesBinarized.Select(im => im.ImageBytes);
+            var neuralNet = new NeuralNet(0.80M, imagesBinarizedBytes.First().Length);
+            foreach (var image in imagesBinarizedBytes)
+            {
+                neuralNet.Process(image);
+            }
+
+            Clusters = neuralNet.Clusters
+                .Select(cluster =>
+                    new ClusterBitmapImage(
+                        cluster.Key.TWeights.ToArray().CreateBitmap().ToBitmapImage(),
+                        cluster.Select(element => element.TWeights.ToArray().CreateBitmap().ToBitmapImage())))
+                .ToList();
+
             //ImageSourceBefore = new Bitmap("Images/iconmonstr-generation-10-16.jpg").ToBitmapImage();
             //CustomPixelImage = RandomBytes().CreateBitmap().ToBitmapImage();
 
